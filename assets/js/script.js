72b0fb4ae2f7b35911092334ebdda786740c3adc
himanshusindhal <101419666+himanshusindhal@users.noreply.github.com>
2026-05-06 18:35:05 +0530

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-/**
- * Script for WhatsApp Order Button
- * Keeps dynamic DOM clean
- */
-document.addEventListener('DOMContentLoaded', function() {
-    // We don't necessarily need to attach JS events if simple links suffice
-    // But this file is enqueued if we need dynamic behavior (like updating variable product links) in future versions.
-});
